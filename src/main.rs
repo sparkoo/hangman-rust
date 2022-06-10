ac6067881a860ac43584903bc2a364ae970ea21b
Michal Vala <michal.vala@gmail.com>
2022-06-10 06:21:56 +0200

    enter prompt so so fine

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,45 @@ use std::io::{Read, Write};
 
 fn main() -> io::Result<()> {
     let word = pick_word();
-
-    let entered = prompt();
-
-    println!("bla '{}'", entered);
+    game_loop(&word);
     Ok(())
+}
+
+fn game_loop(word: &String) {
+    loop {
+        let entered = match prompt() {
+            Ok(v) => {
+                v
+            }
+            Err(err) => {
+                println!("err '{}'", err);
+                continue;
+            }
+        };
+    }
 }
 
 fn pick_word() -> String {
     "blabol".to_string()
 }
 
-fn prompt() -> u8 {
-    let mut buffer = vec![];
-
+fn prompt<'a>() -> Result<u8, String> {
     print!("Enter letter: ");
     stdout().flush().unwrap();
-    let entered = stdin().bytes().next().and_then(|r| r.ok());
-    match entered {
-        Ok(e) => {
-            println!("{}", e)
+
+    let mut buffer = String::new();
+    match stdin().read_line(&mut buffer) {
+        Ok(n) => {
+            if n != 2 {
+                return Err("enter single letter".to_string());
+            }
+            return match buffer.as_bytes()[0] {
+                l if l >= 97 && l <= 122 => Ok(l),
+                _ => Err("enter [a-z]".to_string()),
+            };
         }
-        Err(_) => {
-            println!("failed")
+        Err(n) => {
+            return Err(n.to_string());
         }
     }
-
-    entered.unwrap().unwrap()
 }
